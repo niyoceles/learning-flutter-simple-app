f82c2dc19a2d94f99ceb51492686f5b4cb78a497
Celestin Niyonsaba <niyoceles3@gmail.com>
2021-03-31 20:20:22 +0200

    start meal app

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealScreen(this.categoryId, this.categoryTitle);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(categoryTitle)),
+      body: Center(
+        child: Text('Meal page'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapplication/categories_screen.dart';
 import 'package:myapplication/constants/constants.dart';
 // import 'package:myapplication/pages/home_page.dart';
 import 'package:myapplication/pages/home_page_with_fb.dart';
@@ -18,12 +19,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Constants.prefs.getBool("loggedIn") == true ? HomePageFB(): LoginPage(),
-        routes:{
-          LoginPage.routeName: (content)=> LoginPage(),
-          HomePageFB.routeName: (content)=> HomePageFB(),
-          TodoScreen.routeName: (content)=> TodoScreen(),
-        });
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+        home: CategoriesScreen()
+        // Constants.prefs.getBool("loggedIn") == true ? CategoriesScreen(): LoginPage(),
+        // routes:{
+        //   CategoriesScreen.routeName: (content)=> CategoriesScreen(),
+        //   LoginPage.routeName: (content)=> LoginPage(),
+        //   HomePageFB.routeName: (content)=> HomePageFB(),
+        //   TodoScreen.routeName: (content)=> TodoScreen(),
+        // }
+        );
   }
 }
